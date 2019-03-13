7304ae809cdfa71670bc6e57d5d1d2302c8e1074
Max Muir <mmuir@pillartechnology.com>
2019-03-13 13:20:20 -0400

    Refactored test class for maximum use of language and framework

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -3,10 +3,28 @@ package tddmicroexercises.tirepressuremonitoringsystem;
 
 import mockClasses.HighPressureSensor;
 import mockClasses.LowPressureSensor;
+import mockClasses.NormalPressureSensor;
 import org.junit.*;
+
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 public class TestAlarm {
+
+    Random random = new Random();
+    private final double LowPressureThreshold = 17;
+    private final double HighPressureThreshold = 21;
+    ISensor mockSensor;
+
+    @Before
+    public void setUp() throws Exception {
+        double psi = random.nextDouble()*22;
+        mockSensor = generateISensor(psi);
+    }
+
+
+
 
     @Test
     public void WhenInstantiatingIsNotNull() {
@@ -18,7 +36,7 @@ public class TestAlarm {
 
     @Test
     public void WhenCheckingLowPressureAlarmSounds() {
-        ISensor mockSensor = new LowPressureSensor();
+        ISensor mockSensor = generateISensor(5);
         Alarm alarm = new Alarm(mockSensor);
 
         alarm.check();
@@ -28,11 +46,50 @@ public class TestAlarm {
 
     @Test
     public void WhenCheckingHighPressureAlarmSounds() {
-        ISensor mockSensor = new HighPressureSensor();
+        ISensor mockSensor = generateISensor(22);
         Alarm alarm = new Alarm(mockSensor);
 
         alarm.check();
 
         assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void WhenCheckingNormalPressureNoAlarm() {
+        ISensor mockSensor = generateISensor(17);
+        Alarm alarm = new Alarm(mockSensor);
+
+        alarm.check();
+
+        assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void WhenCheckingRandomPressureAlarmRespondsCorrectly(){
+        String sensorType = mockSensor.getClass().toString();
+
+        Alarm alarm = new Alarm(mockSensor);
+
+        alarm.check();
+
+        switch(sensorType.toCharArray()[0]){
+            case 'N':
+                assertFalse(alarm.isAlarmOn());
+                break;
+            case 'L':
+            case 'H':
+                assertTrue(alarm.isAlarmOn());
+
+        }
+
+    }
+
+    private ISensor generateISensor(double psi){
+        if(psi < LowPressureThreshold){
+            return new LowPressureSensor();
+        } else if (psi > HighPressureThreshold){
+            return new HighPressureSensor();
+        }
+        return new NormalPressureSensor();
     }
 }
